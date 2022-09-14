9ab262a579861987fac52814f41a381d6b449a11
Subramanian Murugan <sasubramanian_md@hotmail.com>
2022-09-14 17:20:35 +0530

    npm scripts express

--- myexpressapp/src/routers/customer.router.js
+++ myexpressapp/src/routers/customer.router.js
@@ -4,14 +4,30 @@ const customerRouter = express.Router()
 customerRouter.get('/', (req, res) => {
     res.end('Customers-Get!!!')
 })
+customerRouter.get('/:id', (req, res) => {
+    const params = req.params
+    console.log(params)
+    res.end('Customers-Get!!!' + params.id)
+})
+customerRouter.get('/search/bycity', (req, res) => {
+    const params = req.query
+    console.log(params)
+    res.end('Customers-search!!!')
+})
+
+
 customerRouter.post('/create', (req, res) => {
     res.end('Customers-POST')
 })
-customerRouter.put('/update', (req, res) => {
+
+
+customerRouter.put('/update/:id', (req, res) => {
+    const params = req.params
+    console.log(params)
     res.end('Customers-UPDATE')
 })
-customerRouter.delete('/remove', (req, res) => {
-    res.end('Customers-Delete')
+customerRouter.delete('/remove/:id', (req, res) => {
+      res.end('Customers-Delete')
 })
 //
 module.exports = customerRouter;
